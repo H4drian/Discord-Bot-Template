2fb9878c57730ad0285a6eff26f6270659bab66e
Icosa <117495728+H4drian@users.noreply.github.com>
2023-12-18 00:07:12 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
 import 'dotenv/config';
 import commands from './commands';
+import {} from './events';
 
 config();
 
